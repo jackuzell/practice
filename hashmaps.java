79ceac59fbc9938dd4238e877bd2adec08b13a07
jackuzell <Jackuzell05@gmail.com>
2025-09-24 15:23:25 +0100

    Good cheat sheet for understanding the use of hashmaps"

---
+++ hashmaps.java
@@ -0,0 +1,25 @@
+import java.util.HashMap;
+public class hashmaps
+{
+    public static void main(String[] args) 
+    {
+        /*Hashmaps -
+            A data structure that stores key-value pairs
+            Keys are unique, but values can be duplicated
+            Does not maintain any order, but its memory efficient
+            HashMap<Key, Value>
+         */
+
+         //Creating a HashMap
+         HashMap<String, Double> map = new HashMap<>(); //String is the key and the double is the value.
+
+         //putting things in (put method)
+         map.put("apple",0.50);
+         map.put("banana",0.25);
+         map.put("orange",0.75);
+
+         //removing things (remove method)
+            map.remove("banana");
+         System.out.println(map);
+    }
+}
